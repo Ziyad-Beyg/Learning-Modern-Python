f717ff2a49d0f2779cf3e0cc8a38af784cb189f8
Ziyad-Beyg <beygz17@gmail.com>
2024-01-02 21:21:08 +0500

    class01 remaining code

--- Class_01/main.py
+++ Class_01/main.py
@@ -1,6 +1,10 @@
 print('HELLO, MY NAME IS ZIYAD')
-
-
-
-name: str = 600
+name : str = 600 # this is a type error, because "name" type is string but we initialized it with interger value. Because python is a dunamic language we won't get any error.
 print(name)
+
+
+
+
+
+country: str = "Pakistan Zindabad!"
+print(country)
