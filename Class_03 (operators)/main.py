68667cf0b81272a1c3c5c0f9b4bb417176ffdb8d
Ziyad-Beyg <beygz17@gmail.com>
2024-01-04 21:41:42 +0500

    operators in python

---
+++ Class_03 (operators)/main.py
@@ -0,0 +1,13 @@
+num1 : int = 5
+num2 : int = 8
+
+
+addition : int = num1 + num2 
+subtraction : int = num1 - num2
+multiplication : int = num1 * num2
+division : float = num1 / num2
+
+print(f"5 + 8 = {addition}") # 5 + 8 = 13
+print(f"5 - 8 = {subtraction}") # 5 - 8 = -3
+print(f"5 * 8 = {multiplication}") # 5 * 8 = 40
+print(f"5 / 8 = {division}") # 5 / 8 = 0.625
