daf3a36e16d2a00fa5ee377a1496d6f9040e1925
Ziyad-Beyg <beygz17@gmail.com>
2024-01-05 23:44:37 +0500

    List, indexing, slicing, list methods

---
+++ Class_04 (list, list_methods)/main.py
@@ -0,0 +1,8 @@
+#                                  0           1          2
+teachers_names : list[str] = ["Sir Qasim", "Sir Zia", "Sir Inam"]
+#                                 -3          -2         -1
+
+ 
+print(teachers_names[0]) # Sir Qasim
+
+print(teachers_names[-1]) # Sir Inam
