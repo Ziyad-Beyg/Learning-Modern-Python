a4a077899fb1fd76cfc4505cfa6be56cc5c80443
Ziyad-Beyg <beygz17@gmail.com>
2024-06-30 17:32:03 +0500

    if-elif-else, comprehensive conditions, tuple zip

---
+++ Class_06 (if-elif-else, comprehensive conditions, tuple zip)/main.py
@@ -0,0 +1,9 @@
+number : int = 4
+
+if number >= 5: 
+    print("IF block running") # will run whenever the "if" logic is True
+elif number <= 5:
+    print("ELIF block is running") # will run whenever the "elif" logic is True
+else:
+    print("HELLO WORLD!") # will run if all the above block does not run
+
