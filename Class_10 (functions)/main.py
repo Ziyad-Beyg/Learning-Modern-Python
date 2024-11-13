bba6114722f94901f5f9c775d7822fb7e27e7e09
Ziyad-Beyg <beygz17@gmail.com>
2024-11-13 18:08:01 +0500

    functions

---
+++ Class_10 (functions)/main.py
@@ -0,0 +1,14 @@
+# FUNCTIONS
+#   Three components of a function:
+#       1) Function Definition / Function Signature
+#       2) Function Body
+#       3) Function Calling
+
+
+def my_sum_two_num_function(num1: int, num2: int) -> None:  # function definition
+    # function body starts
+    print(num1 + num2)
+    # function body ends
+
+
+my_sum_two_num_function(2, 4)  # function calling
